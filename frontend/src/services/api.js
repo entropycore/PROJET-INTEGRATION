adb553e0dac8015bc1b83d302e4345055d81dd36
Rime-seria <seriarime@gmail.com>
2026-04-21 21:45:56 +0100

    fix: adoption de la stratégie cookies pour les tokens

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { useAuthStore } from '../stores/auth'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -7,16 +6,6 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-})
-
-api.interceptors.request.use((config) => {
-  const authStore = useAuthStore()
-
-  if (authStore.accessToken) {
-    config.headers.Authorization = `Bearer ${authStore.accessToken}`
-  }//le front envoie Authorization: Bearer <accessToken> si accessToken est présent
-
-  return config
 })
 
 export default api
--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -2,20 +2,17 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    accessToken: null,
     user: null,
     isAuthenticated: false,
   }),
 
   actions: {
     setAuthSession(data) {
-      this.accessToken = data.accessToken
       this.user = data.user
       this.isAuthenticated = true
     },
 
     clearAuthSession() {
-      this.accessToken = null
       this.user = null
       this.isAuthenticated = false
     },
