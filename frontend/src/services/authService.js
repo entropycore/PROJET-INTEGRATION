84f709f8f2c7f77d3785a597ae24f37577379e0f
Rime-seria <seriarime@gmail.com>
2026-04-22 11:46:41 +0100

    fix: alignement de front avec back

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,10 +1,20 @@
 import api from './api'
 
-export const loginRequest = async ({ email, password }) => {
-  const response = await api.post('/auth/login', {
+export const login = async ({ email, password }) => {
+  const response = await api.post('/login', {
     email,
     password,
   })
 
+  return response.data
+}
+
+export const getMe = async () => {
+  const response = await api.get('/me')
+  return response.data
+}
+
+export const logout = async () => {
+  const response = await api.post('/logout')
   return response.data
 }
