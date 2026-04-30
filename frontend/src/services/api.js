12dbe155f66385454d8c951d45b89a02531ae753
Fakir hiba03r <fakirhiba908@gmail.com>
2026-04-30 15:48:52 +0100

    fix(security): add 401 catch and 403 handling in axios interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { useAuthStore } from '../stores/auth'
 const apiBaseUrl = `${import.meta.env.VITE_API_BASE_URL}/api`
 
 const api = axios.create({
@@ -14,6 +14,7 @@ api.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config
+    const authStore = useAuthStore()
 
     if (
       error.response?.status === 401 &&
@@ -21,10 +22,19 @@ api.interceptors.response.use(
       originalRequest.url !== '/auth/refresh-token'
     ) {
       originalRequest._retry = true //je retente une fois apres avoir renouvelé le token
-      await api.post('/auth/refresh-token')
-      return api(originalRequest)
+       try {
+         await api.post('/auth/refresh-token')
+         return api(originalRequest)
+       } catch {
+         authStore.clearAuthSession()
+         window.location.href = '/login'
+         return Promise.reject(error)
+}
     }
-
+    // 403 — Accès refusé
+    if (error.response?.status === 403) {
+      window.location.href = '/403'
+    }
     return Promise.reject(error)
   }
 )
