35b1bc077c13536c58686fad956d2da697161908
douae07-coder <douae010100@gmail.com>
2026-05-06 18:13:57 +0100

    feat: importation des componenets necessaires

--- frontend/src/views/admin/Reports.vue
+++ frontend/src/views/admin/Reports.vue
@@ -1,5 +1,9 @@
-<template>
-  <section>
-    <h1>Signalements</h1>
-  </section>
-</template>
+<script setup>
+import { computed, ref } from "vue";
+// import { onMounted } from "vue";
+
+import ReportsStats from "@/components/admin/reports/ReportsStats.vue";
+import ReportsToolbar from "@/components/admin/reports/ReportsToolbar.vue";
+import ReportsTable from "@/components/admin/reports/ReportsTable.vue";
+import ReportDetailsModal from "@/components/admin/reports/ReportDetailsModal.vue";
+</script>
