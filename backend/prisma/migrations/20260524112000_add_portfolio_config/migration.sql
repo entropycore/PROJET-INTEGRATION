d77e47c9efcf0c883a5f0f0ce7ca17d1768bf9b0
mohamed zaaboul <mohamedzaaboul56@gmail.com>
2026-05-24 18:24:58 +0200

    fixe: base donner migration et uploads fichier csv

--- backend/prisma/migrations/20260524112000_add_portfolio_config/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "portfolios" ADD COLUMN "theme" VARCHAR(80) NOT NULL DEFAULT 'modern-academic';
-ALTER TABLE "portfolios" ADD COLUMN "included_sections" JSONB NOT NULL DEFAULT '[]';
-ALTER TABLE "portfolios" ADD COLUMN "included_items" JSONB NOT NULL DEFAULT '{}';
