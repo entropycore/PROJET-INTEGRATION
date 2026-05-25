1c24f36b4e8c61e881a518d87c795d4b68f1d37b
Hossam Ouammi <ouammi.hossam@gmail.com>
2026-05-25 18:13:50 +0100

    sync backend from back-frontend

--- backend/tests/integration/controllers/authController.test.js
+++ backend/tests/integration/controllers/authController.test.js
@@ -32,14 +32,7 @@ describe('INTEGRATION TEST: Auth Controller - Quality Assurance Suite', () => {
 
             const res = await request(app)
                 .post('/api/auth/register')
-                .send({ 
-                    email: 'najim.qa@ensa.ma', 
-                    password: 'Password123!', 
-                    firstName: 'Najim', 
-                    lastName: 'QA',
-                    company: 'ENSA Tanger',
-                    jobTitle: 'Lead QA'      
-                });
+                .send({ email: 'najim.qa@ensa.ma', password: 'Password123!', firstName: 'Najim', lastName: 'QA' });
 
             expect(res.statusCode).toBe(201); // Succès de création
             expect(res.body.message).toMatch(/Vérifiez votre email/i);
@@ -55,13 +48,11 @@ describe('INTEGRATION TEST: Auth Controller - Quality Assurance Suite', () => {
                     email: 'existing@ensa.ma', 
                     password: 'Password123!',
                     firstName: 'Najim', 
-                    lastName: 'QA' ,
-                    company: 'ENSA Tanger',
-                    jobTitle: 'Lead QA'
+                    lastName: 'QA' 
                 });
 
-            expect(res.statusCode).toBe(409); // Erreur client : conflit
-            expect(res.body.message).toMatch(/Cet email est déjà utilisé/i);
+            expect(res.statusCode).toBe(400); // Erreur client : conflit
+            expect(res.body.message).toMatch(/déjà utilisé/i);
         });
 
         // TC-AUTH-03 (Security)
