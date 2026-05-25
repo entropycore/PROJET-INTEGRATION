da60bbca759db744105a3dd60d900d25c910e2a3
Hossam Ouammi <ouammi.hossam@gmail.com>
2026-05-25 18:15:01 +0100

    make backend match back-frontend

--- backend/src/services/administrator/mappers/validationMappers.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  ...require('./validationRequestMappers'),
-  ...require('./projectInternshipValidationMappers'),
-  ...require('./legacyValidationMappers'),
-};
